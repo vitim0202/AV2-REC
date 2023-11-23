bdd2d0bf9c0d92b318eb4421f3542c1b8c757e18
vitor <105921994+vitim0202@users.noreply.github.com>
2023-11-23 07:33:03 -0300

    Add files via upload

---
+++ av2terceirotri-main/main.js
@@ -0,0 +1,16 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
+
+}
